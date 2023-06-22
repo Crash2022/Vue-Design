bc51d844832872e332b85e524a90af0e2f5e72d1
Crash2022 <crash555@mail.ru>
2023-06-22 15:47:27 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,77 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <navbar/>
+    <div class="app">
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
 </template>
 
+<script setup lang="ts">
+
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Google Sans, sans-serif;
+}
+.app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    margin: 0 auto;
+    background-color: #f6f6f6;
+    //overflow-x: hidden;
+
+    .container {
+        //border: 1px solid black;
+        width: 100%;
+        max-width: 900px;
+    }
 }
 
-nav {
-  padding: 30px;
+/*---------------scrollbar---------------*/
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: #ffffff;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+::-webkit-scrollbar {
+    width: 12px;
+}
+
+::-webkit-scrollbar-thumb {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: cornflowerblue;
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background-color: grey;
+    opacity: 0.5;
+}
+
+.app::-webkit-scrollbar {
+    width: 10px;
+    height: 10px;
+}
+
+/* Track */
+.app::-webkit-scrollbar-track {
+    background: #ffffff;
+}
+
+/* Handle */
+.app::-webkit-scrollbar-thumb {
+    background: cornflowerblue;
+}
+
+/* Handle on hover */
+.app::-webkit-scrollbar-thumb:hover {
+    border: 2px solid cornflowerblue;
 }
 </style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="home_page">
+        <div class="title">
+            <h1>Добро пожаловать в приложение</h1>
+            <h1>"Любимые фильмы"</h1>
+        </div>
+        <div class="main_image">
+            <img :src="MainImage" alt="main-image">
+        </div>
+    </div>
+</template>
+
+<script>
+import MainImage from '@/shared/assets/main-image-01.jpg'
+export default {
+    // импорт изображения как объект
+    data: function () {
+        return {
+            MainImage
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.home_page {
+    padding: 20px;
+    height: calc(100vh - 50px);
+
+    .title {
+        margin: 30px 0;
+        text-align: center;
+
+        h1 {
+            line-height: 50px;
+        }
+    }
+    .main_image {
+        //width: 100%;
+        text-align: center;
+
+        img {
+            // вариант с оригинальным размером изображения
+            //height: auto;
+            //max-width: 100%;
+
+            height: auto;
+            max-width: 600px;
+            width: 100%;
+        }
+    }
+}
+</style>
