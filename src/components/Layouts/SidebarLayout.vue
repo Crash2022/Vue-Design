d492c9613c37e32bf09d5fd4a3c5d03a82b85627
Crash2022 <crash555@mail.ru>
2023-06-23 12:27:39 +0500

    fix: links styles

--- src/components/Layouts/SidebarLayout.vue
+++ src/components/Layouts/SidebarLayout.vue
@@ -37,24 +37,28 @@ const links = ref([
     background: #fff;
     padding: 20px;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.07);
-    //transition: 0.2s;
+    transition: 0.2s;
     //transform: translateX(-250px);
+    transform: translateX(0px);
 
     //&_isopen {
     //    transform: translateX(0px);
     //}
     //
-    //&__link {
-    //    display: block;
-    //    border-radius: 12px;
-    //    border: 2px solid #fff;
-    //    transition: 0.2s;
-    //    font-weight: bold;
-    //    margin-bottom: 10px;
-    //
-    //    &:hover {
-    //        color: var(--primary);
-    //    }
-    //}
+    &__link {
+        display: block;
+        border-radius: 12px;
+        padding: 10px;
+        border: 1px solid #d5d8ff;
+        //transition: 0.1s;
+        font-weight: bold;
+        margin-bottom: 10px;
+
+        &:hover {
+            //border-color: var(--primary);
+            border: 2px solid var(--primary);
+            padding: 9px;
+        }
+    }
 }
 </style>
