8fdf9165372a53f19e69814e9eec8f6bb946c388
Crash2022 <crash555@mail.ru>
2023-06-23 14:51:47 +0500

    add: button outlined v2

--- src/pages/ButtonPage.vue
+++ src/pages/ButtonPage.vue
@@ -1,35 +1,42 @@
 <template>
     <div class="buttons_page">
-        <CustomButton @click="buttonClick">
-            Button Primary
-        </CustomButton>
-        <CustomButton @click="buttonClick" :color="'secondary'">
-            Button Secondary
-        </CustomButton>
-        <CustomButton @click="buttonClick" :color="'success'">
-            Button Success
-        </CustomButton>
-        <CustomButton @click="buttonClick" :color="'info'">
-            Button Info
-        </CustomButton>
-        <CustomButton @click="buttonClick" :color="'warning'">
-            Button Warning
-        </CustomButton>
-        <CustomButton @click="buttonClick" :color="'danger'">
-            Button Danger
-        </CustomButton>
+        <div class="heading_2">Colors</div>
+        <div>
+            <CustomButton @click="buttonClick">
+                Button Primary
+            </CustomButton>
+            <CustomButton @click="buttonClick" :color="'secondary'">
+                Button Secondary
+            </CustomButton>
+            <CustomButton @click="buttonClick" :color="'success'">
+                Button Success
+            </CustomButton>
+        </div>
+        <div>
+            <CustomButton @click="buttonClick" :color="'info'">
+                Button Info
+            </CustomButton>
+            <CustomButton @click="buttonClick" :color="'warning'">
+                Button Warning
+            </CustomButton>
+            <CustomButton @click="buttonClick" :color="'danger'">
+                Button Danger
+            </CustomButton>
+        </div>
 
-        <CustomButton @click="buttonClick" :rounded="true">
-            Button Primary Rounded
-        </CustomButton>
+        <div class="heading_2">Disabled, Rounded, Outlined</div>
+        <div>
+            <CustomButton @click="buttonClick" disabled>
+                Button Primary Disabled
+            </CustomButton>
+            <CustomButton @click="buttonClick" :rounded="true">
+                Button Primary Rounded
+            </CustomButton>
 
-        <CustomButton @click="buttonClick" :outlined="true">
-            Button Outlined
-        </CustomButton>
-
-        <CustomButton @click="buttonClick" disabled>
-            Button Primary Disabled
-        </CustomButton>
+            <CustomButton @click="buttonClick" :outlined="true">
+                Button Outlined
+            </CustomButton>
+        </div>
     </div>
 </template>
 
@@ -44,6 +51,8 @@ const buttonClick = () => {
 <style lang="scss" scoped>
 .buttons_page {
     > div {
+        display: flex;
+        gap: 10px;
         margin-bottom: 10px;
     }
 }
--- src/pages/TypographyPage.vue
+++ src/pages/TypographyPage.vue
@@ -1,27 +1,27 @@
 <template>
     <h1>h1 default</h1>
-    <h2 class="heading-1">Heading 1</h2>
-    <h2 class="heading-2">Heading 2</h2>
-    <h2 class="heading-3">Heading 3</h2>
+    <h2 class="heading_1">Heading 1</h2>
+    <h2 class="heading_2">Heading 2</h2>
+    <h2 class="heading_3">Heading 3</h2>
 </template>
 
 <script setup>
 
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .heading {
-    &-1 {
+    &_1 {
         font-size: 34px;
         margin: 20px 0;
         font-weight: 300;
     }
-    &-2 {
+    &_2 {
         font-size: 22px;
         margin: 20px 0;
         font-weight: 300;
     }
-    &-3 {
+    &_3 {
         font-size: 17px;
         margin: 20px 0;
         font-weight: 300;
