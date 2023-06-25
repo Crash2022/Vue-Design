8fe1b20b620b0f1a3bb6305411e634aad5ff6bb4
Crash2022 <crash555@mail.ru>
2023-06-25 15:34:52 +0500

    add: ProgressBar flow

--- src/pages/ProgressBarPage.vue
+++ src/pages/ProgressBarPage.vue
@@ -4,16 +4,22 @@
     <custom-progress-bar percent="30" max-width="400px" color="success"/>
     <custom-progress-bar percent="50" max-width="500px" color="warning"/>
 
-    <custom-progress-bar percent="50" max-width="600px"/>
-    <custom-button @click="addProgressPercent" :label="'Добавить 10%'"/>
+    <custom-progress-bar :percent="currentPercent" max-width="600px"/>
+    <custom-button @click="addProgressPercent"
+                   :label="'Добавить 10%'"
+                   :disabled="currentPercent === 100"
+    />
 </template>
 
 <script setup>
 import CustomProgressBar from '@/shared/ui/CustomUI/CustomProgressBar.vue'
 import CustomButton from '@/shared/ui/CustomUI/CustomButton.vue'
+import {ref} from 'vue'
 
+const currentPercent = ref(10)
 const addProgressPercent = () => {
-
+    if (currentPercent.value === 100) return
+    currentPercent.value += 30
 }
 </script>
 
