e905adb4eb2d9efb037b82c221ff0975b924e1b0
Crash2022 <crash555@mail.ru>
2023-06-25 15:15:50 +0500

    refactor: ProgressBar v2

--- src/pages/ProgressBarPage.vue
+++ src/pages/ProgressBarPage.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="heading_2">Progress Bar</div>
-    <div>
-        <custom-progress-bar percent="50" max-width="500px"/>
-    </div>
+    <custom-progress-bar percent="10" max-width="300px"/>
+    <custom-progress-bar percent="30" max-width="400px" color="success"/>
+    <custom-progress-bar percent="50" max-width="500px" color="warning"/>
 </template>
 
 <script setup>
