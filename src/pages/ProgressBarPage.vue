9339b4b11da75efd72f780ec1499fbc9ca096a59
Crash2022 <crash555@mail.ru>
2023-06-25 16:55:28 +0500

    refactor: ProgressPage v4

--- src/pages/ProgressBarPage.vue
+++ src/pages/ProgressBarPage.vue
@@ -30,26 +30,29 @@
     <div class="progress_buttons">
         <custom-button @click="startTimerCircleProgressPercent"
                        :label="'Старт'"
-                       :disabled="currentTimerCirclePercent === 100"
+                       :disabled="currentTimerCirclePercent === 100 || progressInterval !== undefined"
         />
         <custom-button @click="stopTimerCircleProgressPercent"
                        :label="'Стоп'"
         />
         <custom-button @click="clearTimerCircleProgressPercent"
                        :label="'Сброс'"
+                       :disabled="currentTimerCirclePercent === 0"
         />
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import CustomProgressBar from '@/shared/ui/CustomUI/CustomProgressBar.vue'
 import CustomButton from '@/shared/ui/CustomUI/CustomButton.vue'
 import CustomProgressCircle from '@/shared/ui/CustomUI/CustomProgressCircle.vue'
-import {ref} from 'vue'
+import {ref, onMounted} from 'vue'
 
-const currentPercent = ref(10)
-const currentCirclePercent = ref(25)
-const currentTimerCirclePercent = ref(0)
+const currentPercent = ref<number>(10)
+const currentCirclePercent = ref<number>(25)
+const currentTimerCirclePercent = ref<number>(0)
+// const currentTimerCirclePercentButton = ref(true)
+let progressInterval = ref<number | undefined>(undefined)
 
 const addProgressPercent = () => {
     if (currentPercent.value === 100) return
@@ -67,22 +70,33 @@ const clearCircleProgressPercent = () => {
     currentCirclePercent.value = 0
 }
 
-let progressInterval;
+// let progressInterval;
 const startTimerCircleProgressPercent = () => {
-    if (currentCirclePercent.value > 100) {
-        clearInterval(progressInterval)
-        return
-    }
-    progressInterval = setInterval(() => {
+    // if (currentCirclePercent.value === 100) {
+    //     clearInterval(progressInterval)
+    //     return
+    // }
+    progressInterval.value = setInterval(() => {
         currentTimerCirclePercent.value += 50
     }, 1000)
 }
 const stopTimerCircleProgressPercent = () => {
-    clearInterval(progressInterval)
+    clearInterval(progressInterval.value)
 }
 const clearTimerCircleProgressPercent = () => {
     currentTimerCirclePercent.value = 0
+    progressInterval.value = undefined
 }
+
+// onMounted(() => {
+//         // if (currentCirclePercent.value > 100) {
+//         //     clearInterval(progressInterval.value)
+//         //     return
+//         // }
+//         progressInterval = setInterval(() => {
+//             currentTimerCirclePercent.value += 50
+//         }, 1000)
+// })
 </script>
 
 <style lang="scss" scoped>
