e0d958360aaacd3d257a805e1864c9575f6115c7
Crash2022 <crash555@mail.ru>
2023-06-25 15:47:34 +0500

    add: ProgressCircle flow

--- src/pages/ProgressBarPage.vue
+++ src/pages/ProgressBarPage.vue
@@ -14,20 +14,42 @@
                        :label="'Сбросить до 10%'"
         />
     </div>
+
+    <div class="heading_2">Progress Circle</div>
+    <custom-progress-circle :percent="currentCirclePercent" color="danger"/>
+    <div class="progress_buttons">
+        <custom-button @click="addCircleProgressPercent"
+                       :label="'Добавить 10%'"
+                       :disabled="currentPercent === 100"
+        />
+        <custom-button @click="clearCircleProgressPercent"
+                       :label="'Сбросить'"
+        />
+    </div>
 </template>
 
 <script setup>
 import CustomProgressBar from '@/shared/ui/CustomUI/CustomProgressBar.vue'
 import CustomButton from '@/shared/ui/CustomUI/CustomButton.vue'
+import CustomProgressCircle from '@/shared/ui/CustomUI/CustomProgressCircle.vue'
 import {ref} from 'vue'
 
 const currentPercent = ref(10)
+const currentCirclePercent = ref(0)
 const addProgressPercent = () => {
     if (currentPercent.value === 100) return
     currentPercent.value += 30
 }
 const clearProgressPercent = () => {
     currentPercent.value = 10
+}
+
+const addCircleProgressPercent = () => {
+    if (currentCirclePercent.value === 100) return
+    currentCirclePercent.value += 25
+}
+const clearCircleProgressPercent = () => {
+    currentCirclePercent.value = 0
 }
 </script>
 
